367687d206ba0c82e38b745637c05ed8e9802c53
Ali Hummos <34151594+hummosa@users.noreply.github.com>
2020-10-27 18:28:58 -0700

    Update models.py

--- models.py
+++ models.py
@@ -36,68 +36,6 @@ class MD(Layer_model):
     '''
     pass
 
-# classes for training, testing, experimenting or running any simulations ...
-
-class Train_model():
-    """
-    Class to hold model training procedure parameters and algorithm..
-    """
-    def __init__(self, parameter_list):
-        """
-        Initialize class with default parametes
-        """
-
-        raise NotImplementedError
-
-    def train(self, model, Ntrain):
-        """
-        trains model model for Ntrain iterations
-        """
-        for traini in range(Ntrain):
-            pass
-
-        return model #return trained model
-
-
-class Test_model():
-    """
-    Test the performance of a model, also plots exemplar neuronal responses for a given list of input combinations
-    """
-
-    def __init__(self, parameter_list):
-        """
-        Initialize class with default parametes
-        """
-        raise NotImplementedError
-
-    def test(self, model, Ntest):
-        """
-        tests model model for Ntest iterations
-        """
-        for testi in range(Ntest):
-            pass# classes for training, testing, experimenting or running any simulations ...
-
-class Train_model():
-    """
-    Class to hold model training procedure parameters and algorithm..
-    """
-    def __init__(self, parameter_list):
-        """
-        Initialize class with default parametes
-        """
-
-        raise NotImplementedError
-
-    def train(self, model, Ntrain):
-        """
-        trains model model for Ntrain iterations
-        """
-        for traini in range(Ntrain):
-            pass
-
-        return model #return trained model
-
-
 class Model():
     """
     Defines a number of areas or layers
