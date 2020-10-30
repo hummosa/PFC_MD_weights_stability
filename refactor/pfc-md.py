1eb212e7acd6b7f2c0f215dc4d7ce8c70f65656e
Ali Hummos <hummosa@gmail.com>
2020-10-30 15:16:15 -0400

    basic pfc-md model and config (should go to separate file)

--- refactor/pfc-md.py
+++ refactor/pfc-md.py
@@ -4,18 +4,29 @@ from network import Network
 import models
 import torch
 
+import sys,shelve, tqdm, time
+# from data_generator import data_generator
+from plot_figures import monitor
+
+
 class Config():
-    def __init__():
+    def __init__(self):
+        #enviroment parameters:
         config.debug = False
         config.saveData = True
         config.RNGSEED = 1                    # Seed for the np random number generator
         np.random.seed([config.RNGSEED])
+        config.cuda = False
         # config.args = args_dict               # dict of args label:value
+
+        #Experiment parameters:
         config.Ntasks = 2                     # Ambiguous variable name, replacing with appropriate ones below:  # number of contexts 
         config.Ncontexts = 2                  # number of contexts (match block or non-match block)
         config.Nblocks = 4                    # number of blocks for the simulation
 
-        config.Ncues = 4 #config.Ncontexts *2   # number of input cues. Two for up cue, and two for down cue.
+        #Network architecture
+        config.Ninputs = 4                      # total number of inputs
+        config.Ncues = 2                     # How many of the inputs are task cues (UP, DOWN)
         config.Nmd    = 2                       # number of MD cells.
         config.Npfc = 1000                      # number of pfc neurons
         config.Nofc = 1000                      # number of ofc neurons
@@ -53,8 +64,6 @@ class Config():
 
         config.positiveRates = True           # whether to clip rates to be only positive, G must also change
 
-        config.MDlearn = True                # whether MD should learn
-
         config.reinforce = True              # use reinforcement learning (node perturbation) a la Miconi 2017
         config.MDreinforce = True            #  instead of error-driven learning
                                             
@@ -84,8 +93,7 @@ ofc.recent_error = np.zeros(config.Ncontexts)           # Recent reward accumula
 ofc.recent_error_history = []  # List to keep track of entire error history
 
 
-
-
+# Initialize weights
 wPFC2MD = np.random.normal(size=(config.Nmd, config.Npfc))\
            *config.G/np.sqrt(config.Nsub*2)
 wPFC2MD -= np.mean(wPFC2MD,axis=1)[:,np.newaxis] # same as res rec, substract mean from each row.
@@ -94,18 +102,29 @@ wMD2PFC = np.random.normal(size=(config.Npfc, config.Nmd))\
 wMD2PFC -= np.mean(wMD2PFC,axis=1)[:,np.newaxis] # same as res rec, substract mean from each row.
 wMD2PFCMult = wMD2PFC # Get the exact copy to init mult weights
 initial_norm_wPFC2MD = np.linalg.norm(wPFC2MD)
-initial_norm_wMD2PFC = np.linalg.norm(wMD2PFC)
-MDpreTrace = np.zeros(shape=(config.Npfc))
+initial_norm_wMD2PFC = np.linalg.norm(wMD2PFC) # Keep the initial norm for synaptic scaling later.
+MDpreTrace = np.zeros(shape=(config.Npfc)) # Used to hold pre activity for Hebbian learning at MD-PFC synapses
 
 Jrec = np.random.normal(size=(config.Npfc, config.Npfc))\
             *config.G/np.sqrt(config.Nsub*2)
-if cuda:
+if config.cuda:
     Jrec = torch.Tensor(Jrec).cuda()
 
+# W_in: brreak down into cues, which get projected to 200 neurons, and then to v1 v2 inputs, which we'll spread using a gaussian. I see all being a gaussian eventually but for now let's keep some structure
+Cues_wIn = np.zeros((config.Npfc,config.Ncues))
+config.cueFactor = 0.75#1.5 Ali halved it when I added cues going to both PFC regions, i.e two copies of input. But now working ok even with only one copy of input.
+if config.positiveRates: lowcue,highcue = 0.5,1.   #NOT really sure this applies here. Input weights can be negative!
+else: lowcue,highcue = -1.,1
 
+for cuei in np.arange(config.Ncues):
+    Cues_wIn[config.Nsub*cuei:config.Nsub*(cuei+1),cuei] = \
+            np.random.uniform(lowcue,highcue,size=config.Nsub) \
+                    *config.cueFactor
 
-inputs = np.array([config.Ncues])
-W_in = np.array([1])
+Other_wIn = np.random.normal( size=(config.Npfc,config.Ninputs-config.Ncues) ) * config.cueFactor
+
+inputs = np.array(np.ones(shape(config.Ninputs,1)))
+w_in = np.hstack((Cues_wIn, Other_wIn))
 W_pfc_md = wPFC2MD
 W_md_pfc = wMD2PFC
 
