bb73826c4249be70fb98f105b964f4e6a6e0099b
sdrammis <sdrammis@mit.edu>
2020-11-10 19:08:15 -0500

    Added global signaling and broadcast on trial end.
    WIP some sort of averaging in OFC

--- refactor/example.py
+++ refactor/example.py
@@ -8,16 +8,19 @@ import utils
 
 # Network construction
 
-pfc = models.PFC({})
-ofc = models.OFC({})
-output = models.Output({})
+pfc = models.PFC({}, 'PFC')
+ofc = models.OFC({}, 'OFC')
+output = models.Output({}, 'Output')
 
 W_in = np.array([[0, 0], [0, 0]])
 W_ofc_pfc = np.array([[1, 0], [0, 1]])
-W_pfc_output = np.array([1, 1])
+W_pfc_output = np.array([[1, 0], [0, 1]])
 
 
 def update_W_ofc_pfc(step, state, W, ofc, pfc):
+    '''
+    step: ('STEP', timestep) | ('TRIAL_END', global_signals)
+    '''
     return (state, W)
 
 
@@ -25,7 +28,14 @@ def update_W_pfc_output(step, state, W, ofc, pfc):
     return (state, W)
 
 
-network = Network()
+def compute_global_signals(network, expected_output, plasticity):
+    output = network.models['Output'].neurons
+    error = output - expected_output
+    print('output', output, 'expected', expected_output)
+    return {"error": error}
+
+
+network = Network(compute_global_signals)
 network.define_inputs(W_in, pfc)
 network.connect(ofc, pfc, W_ofc_pfc, update_W_ofc_pfc, {})
 network.connect(pfc, output, W_pfc_output, update_W_pfc_output, {})
@@ -44,7 +54,7 @@ def cb(trial_num, step_name, timestep, inp, network):
         dat_inp.append(inp)
 
     if step_name == 'TRIAL_END':
-        for model in network.models:
+        for model in network.models.values():
             if model.name == 'OFC':
                 dat_OFC.append(model.neurons)
             elif model.name == 'PFC':
@@ -62,7 +72,7 @@ def get_input(trial_num, step_name, timestep):
 
 
 def get_output(trial_num, inp):
-    if trial_num < 50:  # Match context
+    if trial_num <= 50:  # Match context
         return inp
     else:
         return 1 - inp
@@ -72,13 +82,34 @@ simulation = Simulation(network)
 trial_setup = [("CUE", 5, False)]
 simulation.run_trials(trial_setup, get_input, get_output, 100, cb)
 
-# TODO finish ploting of data
+# Plot results
+f, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
+# Plot input
 dat_inp = np.array(dat_inp)
-plt.plot(dat_inp)
+match = dat_inp[0:50, :]
+nonmatch = dat_inp[50:100, :]
+x_up_match = np.nonzero(match[:, 0])[0]
+x_down_match = np.nonzero(match[:, 1])[0]
+ax1.plot(x_up_match, np.zeros(len(x_up_match)), 'g^')
+ax1.plot(x_down_match, np.zeros(len(x_down_match)), 'gv')
+x_up_nonmatch = np.nonzero(nonmatch[:, 0])[0] + 50
+x_down_nonmatch = np.nonzero(nonmatch[:, 1])[0] + 50
+ax1.plot(x_up_nonmatch, np.zeros(len(x_up_nonmatch)), 'r^')
+ax1.plot(x_down_nonmatch, np.zeros(len(x_down_nonmatch)), 'rv')
+ax1.set_title('Input')
+# Plot output
+dat_output = np.array(dat_output)
+output_up = np.nonzero(dat_output[:, 0])[0]
+output_down = np.nonzero(dat_output[:, 1])[0]
+ax2.plot(output_up, np.zeros(len(output_up)), 'k^')
+ax2.plot(output_down, np.zeros(len(output_down)), 'kv')
+ax2.set_title('Output')
+# Plot OFC
+dat_OFC = np.array(dat_OFC)
+ax3.plot(dat_OFC)
+ax3.set_title('OFC')
+# Plot PFC
+dat_PFC = np.array(dat_PFC)
+ax4.plot(dat_PFC)
+ax4.set_title('PFC')
 plt.show()
-
-# Plan:
-# [] finish plot of data
-# [] global signal - TRIAL_END output global signals as a dictionary?
-# [] OFC as running average
-# [] run simple simulation w/ plots
--- refactor/models.py
+++ refactor/models.py
@@ -1,23 +1,23 @@
 import uuid
 import numpy as np
+import random
 
 
 class Model:
-    name = ''
 
-    def __init__(self, config):
-        self.uid = uuid.uuid1()
+    def __init__(self, config, name_uid):
+        self.name = ''
         self.neurons = []
         self.weights = []
         self.config = None
 
-    def step(self, xWs):
+    def step(self, xWs, plasticity):
         '''
         xWs: is an input array of [x1W1, x2W2, ...]
         returns an array of firing rates
         '''
 
-    def trial_end(self, xWs):
+    def trial_end(self, global_signals, plasticity):
         '''
         xWs: is an input array of [x1W1, x2W2, ...]
         returns an array of firing rates
@@ -25,69 +25,66 @@ class Model:
 
 
 class MD(Model):
-    name = 'MD'
-
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, config, name_uid):
+        self.name = name_uid
         self.config = config
         self.neurons = [1]  # np.zeros(shape=(config.Nmd))
 
-    def step(self, xWs):
+    def step(self, xWs, plasticity):
         self.neurons = xWs
         print('MD', self.neurons)
         return self.neurons
 
-    def trial_end(self, xWs):
+    def trial_end(self, global_signals, plasticity):
         print('MD trial end called')
         return self.neurons
 
 
 class PFC(Model):
-    name = 'PFC'
-
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, config, name_uid):
+        self.name = name_uid
         self.config = config
         self.neurons = np.zeros(shape=(2, 0))
 
-    def step(self, xWs):
+    def step(self, xWs, plasticity):
         if xWs[0] > xWs[1]:
             self.neurons = np.array([1, 0])
         else:
             self.neurons = np.array([0, 1])
         return self.neurons
 
-    def trial_end(self, xWs):
+    def trial_end(self, global_signals, plasticity):
         return self.neurons
 
 
 class Output(Model):
-    name = 'Output'
 
-    def __init__(self, config):
-        super().__init__(config)
-        self.neurons = np.zeros([0, 0])
+    def __init__(self, config, name_uid):
+        self.name = name_uid
+        self.config = config
+        self.neurons = np.array([0, 0])
 
-    def step(self, xWs):
+    def step(self, xWs, plasticity):
         self.neurons = xWs
         return self.neurons
 
-    def trial_end(self, xWs):
+    def trial_end(self, global_signals, plasticity):
         return self.neurons
 
 
 class OFC(Model):
-    name = 'OFC'
-
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, config, name_uid):
+        self.name = name_uid
         self.config = config
-        # TODO set this to start at 0,0 -- once global signals are working
-        self.neurons = np.array([0.7, 0.3])
+        self.neurons = np.array([0, 0])
 
-    def step(self, xWs):
+    def step(self, xWs, plasticity):
+        # NOTE: If a model has no input,
+        #   the step() function will not be called
         return self.neurons
 
-    def trial_end(self, xWs):
-        # TODO compute average trace
+    def trial_end(self, global_signals, plasticity):
+        # TODO compute reward prediction error and averaging
+        self.neurons = 0.9 * self.neurons - 0.1 * global_signals['error']
+        print('error', global_signals['error'], 'ofc', self.neurons)
         return self.neurons
