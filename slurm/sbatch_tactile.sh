ade6748fb07898c281943e021ca68b25369bda64
Ali Hummos <hummosa@gmail.com>
2021-02-13 11:35:15 -0500

    neural vmPFC edits from OM

--- slurm/sbatch_tactile.sh
+++ slurm/sbatch_tactile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm slurm-*
-for i in 1 30 # OFC switches
+for i in 1 # OFC switches
 # for i in 1. 5. 10. 15. 20. 25. 30. 35. 40. 45. 50. 55. 60. #MD ampflication
 # for i in  1. 1.2 1.3 1.5 #0.2 0.4 0.5 0.7 0.9 G
 do
-  for j in {0..9} # MD LR
+  for j in {0..2} # MD LR
     do
-    for k in 1. #0.6 1.  # MD active
+    for k in .1 0.3 0.4 0.6 0.7 1. 1.2 1.5 #0.6 1.  # MD active
     do
      sbatch ./slurm/run_tactile.sh $1 $i $j $k
     done
